c6341bcf90a1cdf0f91b56778c5c7ac28f4cef27
Fab192 <framirez90304@ufide.ac.cr>
2025-10-16 16:34:52 -0600

    Create Producto.java

---
+++ tienda_vi/src/main/java/com/tienda_vi/domain/Producto.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.tienda_vi.domain;
+
+/**
+ *
+ * @author Fabián
+ */
+public class Producto {
+
+}
